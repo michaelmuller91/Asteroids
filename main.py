c8b789561fdefe21e8788950cf210d771656853e
michaelmuller91 <michael.j.muller91@gmail.com>
2025-07-28 15:11:03 +1000

    updated all python files to include collisions and shots

--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-
+import sys
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
+from shot import *
 
 def main():
     pygame.init
@@ -12,14 +15,31 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides(player):
+                print("Game over!")
+                sys.exit()
+
         screen.fill("black")
-        player.draw(screen)
+        for each in drawable:
+            each.draw(screen)
         pygame.display.flip()
         #limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
